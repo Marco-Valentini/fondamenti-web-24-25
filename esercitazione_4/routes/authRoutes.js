d3323cbd7687a13152f751bec6c9f52a2837000b
Marco Valentini <m.valentini7@phd.poliba.it>
2025-06-04 23:44:53 +0200

    Traccia esercitazione 4 fixed

---
+++ esercitazione_4/routes/authRoutes.js
@@ -0,0 +1,14 @@
+// routes/authRoutes.js
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+
+// POST /api/auth/register
+
+// POST /api/auth/login
+
+// POST /api/auth/refresh (o GET, dipende dalle preferenze, POST è più comune per azioni)
+
+// POST /api/auth/logout
+
+module.exports = router;
